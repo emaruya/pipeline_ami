2d3352204a12e09e8d143673715d51f8fe64ec1f
Erika Maruya <erika.maruya@gmail.com>
2021-11-23 17:39:47 +0000

    alterando hosts

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-img]" > ../ansible/hosts # cria arquivo
-echo "$(terraform output | grep public_ip-dev | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(terraform output | grep public_ip | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
